11728eda6188444fcc7b27c57e0fe578789ceb5e
Nidhi <nidhipandey2698.com>
2022-08-24 11:12:11 +0530

    Added spring boot batch project

---
+++ Spring Batch Demo/spring-batch/spring-batch/src/main/java/com/demo/springbatch/controller/JobController.java
@@ -0,0 +1,43 @@
+package com.demo.springbatch.controller;
+
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/jobs1")
+public class JobController {
+
+
+    @Autowired
+    private JobLauncher jobLauncher;
+
+    @Autowired
+    private Job job;
+
+    @PostMapping("/importCustomers")
+    public void importCsvToDBJob() {
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addLong("startAt", System.currentTimeMillis()).toJobParameters();
+        try {
+            jobLauncher.run(job, jobParameters);
+        } catch (JobExecutionAlreadyRunningException | JobRestartException | JobInstanceAlreadyCompleteException |
+                 JobParametersInvalidException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @GetMapping("/test")
+    public String test(){
+        return "hello";
+    }
+
+}
+
